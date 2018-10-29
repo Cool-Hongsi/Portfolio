45ebdce6236fd9d6321947e3c0fb24b712623207
sungjunhong <hsungjun@myseneca.ca>
2018-10-29 00:46:14 -0400

    Updates

---
+++ precache-manifest.7ebf0e51bb11fbd5f5c357b149e6d910.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "670ac90b4615e5956b8e",
+    "url": "/Portfolio/static/js/main.670ac90b.chunk.js"
+  },
+  {
+    "revision": "002047237c248c93ea5b",
+    "url": "/Portfolio/static/js/1.00204723.chunk.js"
+  },
+  {
+    "revision": "670ac90b4615e5956b8e",
+    "url": "/Portfolio/static/css/main.d6d07469.chunk.css"
+  },
+  {
+    "revision": "002047237c248c93ea5b",
+    "url": "/Portfolio/static/css/1.70b253e1.chunk.css"
+  },
+  {
+    "revision": "59dd2ce35e4f05f0bf2ef6d33e0387e8",
+    "url": "/Portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.9c829404e745640c1233b7cdc4bd77d2.js"
+  "/Portfolio/precache-manifest.7ebf0e51bb11fbd5f5c357b149e6d910.js"
 );
 
 workbox.clientsClaim();
