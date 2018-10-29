1857a4953956e5ad2d21747023be410fad5f2e32
sungjunhong <hsungjun@myseneca.ca>
2018-10-28 22:59:41 -0400

    second commit

---
+++ src/components/landingpage.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Landing extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Landing Page</h1>
+            </div>
+        )
+    }
+}
